b16d1ceefe35c3c1621000d32819eadec1b6e336
mariaflorenciaferreira <florferreirag@gmail.com>
2022-11-02 00:36:58 -0300

    intento solucionar desafio32

--- clase32-desafio/src/routes/logger.routes.js
+++ clase32-desafio/src/routes/logger.routes.js
@@ -7,18 +7,24 @@ const app = express();
 
 
 
-app.get('/*', (req, res) => {
-    logger.info("127.0.0.1 - log info 2");
-    logger.warn("127.0.0.1 - log warn 2");
-    logger.error("127.0.0.1 - log error 2");
+app.get('*', (req, res) => {
+    logger.info(`Se recibio una peticion ${method} a la ruta ${url}`)
+    res.send('no existe la ruta')
+    logger.error('Error al iniciar el servidor')
+    // logger.info("127.0.0.1 - log info 2");
+    // logger.warn("127.0.0.1 - log warn 2");
+    // logger.error("127.0.0.1 - log error 2");
 
 })
 
 
 app.get('/*', (req, res) => {
-    const { url, method } = req
+    // const { url, method } = req
+    // logger.info(`Se recibio una peticion ${method} a la ruta ${url}`)
+    // res.send('no existe la ruta')
     logger.info(`Se recibio una peticion ${method} a la ruta ${url}`)
     res.send('no existe la ruta')
+    logger.error('Error al iniciar el servidor')
 })
 
 
--- clase32-desafio/src/utils/logger.js
+++ clase32-desafio/src/utils/logger.js
@@ -1,24 +1,15 @@
-const log4js=require('log4js')
-const dotenv=require('dotenv')
-
-
-function failRoute(req, res) {
-    const title = 'ROUTING ERROR';
-    const { url, method } = req
-    log4js.warn(`Route ${method} ${url} non-existent`)
-    res.status(404).json( { titulo: title });
-}
+const log4js = require("log4js");
+const dotenv=require('dotenv').config()
 
 log4js.configure({
-
-
+    // appenders: { cheese: { type: "file", filename: "src/logs/cheese.log" } },
+    // categories: { default: { appenders: ["cheese"], level: "error" } },
     appenders: {
-        // defino dos soportes de salida de datos
         consola: { type: 'console' },
         archivoErrores: { type: 'file', filename: 'src/logs/errores.log' },
         archivoWarning: { type: 'file', filename: 'src/logs/warning.log' },
         
-        // defino sus niveles de logueo
+        
         loggerConsola: { type: 'logLevelFilter', appender: 'consola', level: 'info' },
         loggerArchivoErrores: { type: 'logLevelFilter', appender: 'archivoErrores', level: 'error' },
         loggerArchivoWarning: { type: 'logLevelFilter', appender: 'archivoWarning', level: 'warning' }
@@ -30,19 +21,61 @@ log4js.configure({
         prod: {
             appenders: ['loggerConsola', 'loggerArchivoErrores', 'loggerArchivoWarning'], level: 'all'
         }
-    }
+    }    
+});
 
-})
+var logger = log4js.getLogger(process.env.NODE_ENV );
 
 
-let logger=null
 
-if(process.env.NODE_ENV==='production'){
-    console.log(process.env.NODE_ENV)
-    logger=log4js.getLogger('prod')
-}else{
-    logger=log4js.getLogger()
+if (process.env.NODE_ENV === 'production') {
+    logger = log4js.getLogger('prod')
+    console.log( `logger es ${(logger).category}`)
+
+} else {
+    logger = log4js.getLogger()   
+    console.log(`logger es ${(logger).category}`) 
 }
+
+
+// const log4js=require('log4js')
+// const dotenv=require('dotenv')
+
+
+// log4js.configure({
+
+
+//     appenders: {
+
+//         consola: { type: 'console' },
+//         archivoErrores: { type: 'file', filename: 'src/logs/errores.log' },
+//         archivoWarning: { type: 'file', filename: 'src/logs/warning.log' },
+        
+//         // defino sus niveles de logueo
+//         loggerConsola: { type: 'logLevelFilter', appender: 'consola', level: 'info' },
+//         loggerArchivoErrores: { type: 'logLevelFilter', appender: 'archivoErrores', level: 'error' },
+//         loggerArchivoWarning: { type: 'logLevelFilter', appender: 'archivoWarning', level: 'warning' }
+//     },
+//     categories: {
+//         default: {
+//             appenders: ['loggerConsola'], level: 'all'
+//         },
+//         prod: {
+//             appenders: ['loggerConsola', 'loggerArchivoErrores', 'loggerArchivoWarning'], level: 'all'
+//         }
+//     }
+
+// })
+
+
+// let logger=null
+
+// if(process.env.NODE_ENV==='production'){
+//     console.log(process.env.NODE_ENV)
+//     logger=log4js.getLogger('prod')
+// }else{
+//     logger=log4js.getLogger()
+// }
 
 
 module.exports=logger
