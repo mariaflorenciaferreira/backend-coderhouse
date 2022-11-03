ebb6def46ee29b99ba9fb0ba97890b6ede6b7756
mariaflorenciaferreira <florferreirag@gmail.com>
2022-11-03 20:22:57 -0300

    clase36

---
+++ clase36/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const twilio=require('twilio')
+
+const accountSid = process.env.accountSid
+const authToken = process.env.authToken
+const client =twilio(accountSid,authToken)
+
+const options = {
+    body: 'Holasoy un WSP desde Node.js!',
+    mediaUrl: ['https://www.chetu.com/img/twilio/partner/twilio-logo.png'],
+    from: 'whatsapp:+14155238886',
+    to: 'whatsapp:+549'
+}
+
+;(async () => {
+    try {
+        const message = await client.messages.create(options)
+        console.log(message)
+    } catch (error) {
+        console.log(error)
+    }
+})()
