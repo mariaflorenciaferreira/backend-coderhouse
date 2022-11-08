9d8a96d5bcfc93643daefb6baa030be1de940e6f
mariaflorenciaferreira <florferreirag@gmail.com>
2022-11-07 23:02:13 -0300

    registro usuario completo

--- tercerEntrega/src/DB/modelosMongo/userModel.js
+++ tercerEntrega/src/DB/modelosMongo/userModel.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const { string } = require('yargs');
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+    nombreCompleto:String,
+    direccion:String,
+    edad:String,
+    telefono:String
 });
 
 const userModel = mongoose.model('User', userSchema);
--- tercerEntrega/src/controllers/user.controllers/signController.js
+++ tercerEntrega/src/controllers/user.controllers/signController.js
@@ -7,12 +7,16 @@ const getSign = (req, res) => {
 
 
 const postSign = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password,nombreCompleto,direccion,edad,telefono } = req.body;
     
     try {
         const user = {
         email,
         password: createHash(password),
+        nombreCompleto,
+        direccion,
+        edad,
+        telefono
         };
         const createUser = new userModel(user);
         await createUser.save();
