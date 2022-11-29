33f96be264fab328d89e3100b97784265ceea28d
florencia ferreira <florferreirag@gmail.com>
2022-11-29 19:28:43 -0300

    loading msg

--- proyectoFinal/frontend/ecommerce/src/pages/ProductListContainer/ProductListPage.jsx
+++ proyectoFinal/frontend/ecommerce/src/pages/ProductListContainer/ProductListPage.jsx
@@ -5,34 +5,46 @@ import ProductList from "../../component/ProductList/ProductList"
 
 const ProductListPage=()=>{
 
-    // const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
     const [products,setProducts]=  useState([])
 
+
     const getProducts=  async()=>{
-        let responseProducts = await axios ('http://localhost:8080/api/productos')
-        let {data} =responseProducts
-        setProducts(data)
-        // setLoading(false)
+
+        try {
+                let responseProducts = await axios ('http://localhost:8080/api/productos')
+                let {data} =responseProducts
+                setProducts(data)
+
+        } catch (error) {
+            console.log(error)
+        
+        }finally{
+            setLoading(false)
+        }     
     }
+
 
     useEffect(()=>{
         getProducts()
     },[])
 
-    console.log(products)
     return (
 
-            <main>
-        <div className="ItemListContainer">
+        <main>
             
-            <div className="shopSection">
-            <p className="shopTitle">
+            <div className="ItemListContainer">
+                
+                <div className="shopSection">
+                <p className="shopTitle">
 
-                <ProductList products={products.products}/>
-            </p>
+                {loading ?<p>cargando</p> : <ProductList products={products.products}/>}
 
+                    
+                </p>
+
+                </div>
             </div>
-        </div>
         </main>
         
     )
