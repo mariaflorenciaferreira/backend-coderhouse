13a75d86c1e6f61e5d6b4de7f3c61af100543e5e
florencia ferreira <florferreirag@gmail.com>
2022-12-06 20:01:12 -0300

    clase45

---
+++ clase45/my-app/app/Controllers/Http/GetCupcakesController.js
@@ -0,0 +1,15 @@
+// app/Controllers/Http/GetCupcakesController.js
+'use strict'
+
+const Cupcake = use('App/Models/Cupcake')
+
+class GetCupcakesController {
+    
+    async index({ response, view }) {
+        const result = await Cupcake.all()
+        const cupcakes = result.toJSON()
+        return view.render('ListaCupcakes', { cupcakes })
+    }
+}
+
+module.exports = GetCupcakesController
