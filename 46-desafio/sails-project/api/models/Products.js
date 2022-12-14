dfe9858795df02de50e1a438b6ef01b3dbf15089
florencia ferreira <florferreirag@gmail.com>
2022-12-14 20:59:00 -0300

    clase46-desafio

---
+++ 46-desafio/sails-project/api/models/Products.js
@@ -0,0 +1,20 @@
+/**
+ * Products.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    name:{type:'string',required:true},
+    price:{type:'number',required:true},
+    stock:{type:'number',required:true},
+    description:{type:'string'}
+
+  },
+
+};
+
